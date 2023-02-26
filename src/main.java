52a4e7083facf2b0b74e9c0408de60cbf6b64c95
RayZen07 <93152091+RayZen07@users.noreply.github.com>
2023-02-26 22:10:33 +0530

    Update main.java

--- src/main.java
+++ src/main.java
@@ -9,7 +9,9 @@ class Pattern {
 			System.out.println("Press 2 for Triangle/Pyramid Star Pattern");
 			System.out.println("Press 3 for Right Triangle");
 			System.out.println("Press 4 for Right Triangle 2 Star Pattern");
-			System.out.println("Press 5 for Reverse of 4");
+			System.out.println("Press 5 for Reverse of 4 ");
+			System.out.println("Press 6 for Reverse Pyramid ");
+			System.out.println("Press 0 to EXIT");
 			System.out.println("Make your Choice");
 			choice = scanner.nextInt();
 			switch (choice)
@@ -86,6 +88,31 @@ class Pattern {
 						System.out.println();
 					}
 					break;
+					case 6:
+					for (int i = 1; i <= 5; i++)
+				{
+
+					// inner loop to handle number spaces
+					// values changing acc. to requirement
+					for (int j = 1; j <= i; j++) {
+						// printing spaces
+						System.out.print(" ");
+					}
+
+					//  inner loop to handle number of columns
+					//  values changing acc. to outer loop
+					for (int k = 5; k >= i; k--) {
+						// printing stars
+						System.out.print(" "+"*");
+					}
+
+					// ending line after each row
+					System.out.println();
+				}
+					break;
+				case 0:
+					System.exit(0);
+
 
 				default:
 					System.out.println("INVALID CHOICE");
